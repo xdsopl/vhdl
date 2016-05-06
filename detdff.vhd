8b68e1010ff351bfd4079176ed6b64cce5d8a6fe
Ahmet Inan <xdsopl@googlemail.com>
2016-05-06 22:27:42 +0200

    split detdff from quadrature decoder
    created a working detdff and didn't even know .. :)

--- detdff.vhd
+++ detdff.vhd
@@ -1,4 +1,4 @@
--- quadrature_decoder - quadrature decoder without clock input
+-- detdff - Double Edge Triggered Data Flip-Flop
 -- Written in 2016 by <Ahmet Inan> <xdsopl@googlemail.com>
 -- To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
 -- You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
@@ -6,23 +6,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- prior debouncing of a and b is unnecessary
-entity quadrature_decoder is
+entity detdff is
 	port (
-		rotary : in std_logic_vector (1 downto 0);
-		direction : out std_logic;
-		pulse : out std_logic
+		input : in std_logic;
+		clock : in std_logic;
+		output : out std_logic
 	);
-end quadrature_decoder;
+end detdff;
 
-architecture bs of quadrature_decoder is
-	signal a, b : std_logic;
+architecture bs of detdff is
+	signal lo, hi : std_logic;
 begin
-	detdff_inst1 : entity work.detdff
-		port map (rotary(0), rotary(1), a);
-	detdff_inst2 : entity work.detdff
-		port map (rotary(1), rotary(0), b);
-	pulse <= a and b;
-	direction <= b;
+	lo <= input when clock = '0' else lo;
+	hi <= input when clock = '1' else hi;
+	output <= lo when clock = '1' else hi;
 end bs;
 
